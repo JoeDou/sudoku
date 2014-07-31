50b372db2f6dd5f5eb75deffb7626d5ad21eaa5b
JoeDou <joe.dou@gmail.com>
2014-07-30 20:01:37 -0700

    create initial data storage for col, row and section

--- js/gameplay.js
+++ js/gameplay.js
@@ -21,5 +21,36 @@ var matrix =
   [0, 0, 0, 4, 1, 9, 0, 0, 5],
   [0, 0, 0, 0, 8, 0, 0, 7, 9]];
 
+var rowData = [];
+var colData = [];
+var secData = {};
+
+var determineSection = function(col, row){
+  rowSec = Math.floor(row / 3);
+  colSec = Math.floor(col / 3);
+  return [colSec, rowSec];
+};
+
+var initialize = function(){
+  for (var i=0; i<9; i++){
+    rowData.push([]);
+    colData.push([]);
+    var secString = ''+ Math.floor(i/3)+(i%3);
+    secData[secString]=[];
+  }
+  for (var row =0; row < 9; row++){
+    for (var col=0; col < 9; col++){
+      if (matrix[row][col]){
+        rowData[row].push(matrix[row][col]);
+        colData[col].push(matrix[row][col]);
+        var arr = determineSection(col,row);
+        var string = ''+arr[0]+arr[1];
+        secData[string].push(matrix[row][col]);
+      }
+    }
+  }
+};
+
+
 
 
