48331678351894ca596c1322a335c87d23f5c1e9
JoeDou <joe.dou@gmail.com>
2014-07-30 19:11:58 -0700

    jquery to create game board and created a temp matrix to figure out algorithm

--- js/app.js
+++ js/app.js
@@ -1,2 +1,27 @@
 // JavaScript
+$( document ).ready(function() {
+    console.log( "ready!" );
+    createTable();
+});
 
+var createTable = function() {
+  var $td, $tr, $input;
+  var $table = $('<table>');
+  var $gameboard = $('.gameboard');
+
+  // Go over rows
+  for ( var i = 0; i < 9; i++ ) {
+    $tr = $('<tr>');
+    // Go over columns
+    for ( var j = 0; j < 9; j++ ) {
+      // Build the input
+      $input = $('<input>')
+        .attr('maxlength','1')
+        .addClass('inputBox');
+      $td = $('<td>').append($input);
+      $tr.append($td);
+    }
+    $table.append($tr);
+  }
+  $gameboard.append($table);
+};
